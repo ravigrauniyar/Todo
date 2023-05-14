3c3f2db6c134a37686893f531b2a2d71b91ab325
ravigrauniyar <ravi.bim76@hsm.edu.np>
2023-05-14 11:59:16 +0545

    commit

--- app/UserModule/urls.py
+++ app/UserModule/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from .views import TodoAction
 
 urlpatterns = [
-    path('', views.HomePage, name="Home"),
-    path('Login', views.LoginPage, name="Login"),
-    path('View-Todo', views.ViewTodo, name="ViewTodo"),
-    path('Write-Todo', views.WriteTodo, name='WriteTodo'),
-    path('Write-Todo/<int:todo_id>/', views.EditTodo, name='WriteTodo'),
-    path('Delete-Todo/<int:todo_id>/', views.DeleteTodo, name='DeleteTodo'),
+    path('', TodoAction.HomePage, name="Home"),
+    path('Login', TodoAction.LoginPage, name="Login"),
+    path('View-Todo', TodoAction.ViewTodo, name="ViewTodo"),
+    path('Write-Todo', TodoAction.WriteTodo, name='WriteTodo'),
+    path('Write-Todo/<int:todo_id>/', TodoAction.EditTodo, name='WriteTodo'),
+    path('Delete-Todo/<int:todo_id>/', TodoAction.DeleteTodo, name='DeleteTodo'),
 ]
